acb43fb480fc390a92cd5b2139bd18fd7c97e328
Alaa Fayed <alaafayed810@gmail.com>
2024-03-28 20:32:03 +0200

    0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file using puppet
+file { 'school':
+  ensure     => 'present',
+  path       => '/tmp/school',
+  mode       => '0744',
+  owner      => 'www-data',
+  content     => 'I love Puppet'
+}

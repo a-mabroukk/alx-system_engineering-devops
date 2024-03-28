13a9649234fe116e7d4d326e7b4d314ce6f48aa4
Alaa Fayed <alaafayed810@gmail.com>
2024-03-28 22:09:55 +0200

    1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,5 +4,6 @@ file { 'school':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  content => 'I love Puppet'
+  group   =>  'www-data',
+  content => 'I love Puppet',
 }

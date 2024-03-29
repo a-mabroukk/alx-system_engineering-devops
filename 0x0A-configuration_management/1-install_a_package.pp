aa04b9256ffc6ab680ed5abe8c4d3e38c5a3cf04
Alaa Fayed <alaafayed810@gmail.com>
2024-03-29 22:59:12 +0200

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #Installing flask from pip3 by using puppet
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
